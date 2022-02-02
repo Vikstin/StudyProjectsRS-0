0237776cc645b0dec0ca2f7daea60180b6735379
vikstin <stiniaevaviktory@gmail.com>
2022-02-02 21:18:14 +0300

    feat: add function for change img

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,10 @@
-console.log ('1 Вёрстка валидная +10 \n 2 Вёрстка семантическая +20 \n 3 Вёрстка соответствует макету +48 \n 4 Требования к css + 12 \n 5 Интерактивность реализуемая через css +20 \n Итого: 100 баллов');
+console.log ('');
+
+//Change image at click
+
+const portfolioBtn = document.querySelector('.button-gold');
+const portfolioImages = document.querySelectorAll('.photo');
+
+portfolioBtn.addEventListener('click', () => {
+    portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
+});
