be7078aaa7344758887186434a7955519ff933e4
vikstin <stiniaevaviktory@gmail.com>
2022-02-08 03:30:11 +0300

    feat: add in video-player function rewind video

--- portfolio/index.js
+++ portfolio/index.js
@@ -55,10 +55,11 @@ const video = document.querySelector('video.viewer');
 // const controlPlay = document.querySelector('div.play-button');
 const buttonPlay = document.querySelector('div.play');
 const buttonVol = document.querySelector('div.volume');
-const controlPlay_hover = document.querySelector('video.div.play-hover');
-const progress = document.querySelector('input.progress');
+const controlPlay_hover = document.querySelector('div.play-hover');
+const progress = document.querySelector('div.progress');
 // const progressVolume = document.querySelector('input.progress-volume');
 const rangeVol = document.querySelectorAll('input.progress-volume');
+const slider = document.querySelectorAll('input.slider');
 // var drag;
 // var grap;
 // var progression;
@@ -72,7 +73,7 @@ video.addEventListener('timeupdate', updateProgress);
 buttonVol.addEventListener('click', updateButtonVol); 
 rangeVol.forEach(range => range.addEventListener('change', updateVol));
 rangeVol.forEach(range => range.addEventListener('mousemove', updateVol));
-rangeVol.addEventListener('input', changeColor);
+slider.forEach(range => range.addEventListener('input', changeColor)); 
 progress.addEventListener('click', scrub);
 
 function togglePlay() {
@@ -86,10 +87,10 @@ function togglePlay() {
 function updateButtonPlay() {
     if (this.paused) {
         buttonPlay.style.backgroundImage = "url('./assets/video/play.svg')";
-        // controlPlay_hover.classList.toggle('play-hover');
+        controlPlay_hover.classList.toggle('play-hover');
     } else {
         buttonPlay.style.backgroundImage = "url('./assets/video/pause.svg')";
-        // controlPlay_hover.classList.remove('play-hover');
+        controlPlay_hover.classList.remove('play-hover');
     }
 }
 
@@ -99,15 +100,14 @@ function updateVol() {
 }
 
 function updateButtonVol() {
-        if (rangeVol === 0) {
+        if (video.volume === 0) {
         buttonVol.style.backgroundImage = "url('./assets/video/mute.svg')";
-}
-    buttonVol.style.backgroundImage = "url('./assets/video/volume.svg')";
-}
+}    buttonVol.style.backgroundImage = "url('./assets/video/volume.svg')";
+} //no ????
 
 function updateProgress() {
-    var progress = (video.currentTime / video.duration) * 100;
-    progress.style.flexBasic = `${persent}%`;
+    var progress = (video.currentTime / video.duration);
+    progress.style.flexBasis = Math.floor(progress * 1000) / 10 + '%';
 }
 
 function scrub(e) {
@@ -116,10 +116,10 @@ function scrub(e) {
 }
 
 //Change color progress
-progress.addEventListener('input', function() {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #fff ${value}%, white 100%)`
-})
+// progress.addEventListener('input', function() {
+//   const value = this.value;
+//   this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #fff ${value}%, white 100%)`
+// })
 
 function changeColor() {
     const value = this.value;
